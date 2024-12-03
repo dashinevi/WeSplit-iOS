ec21081fe0862cb5332724c16eecfb6c7fdb6af1
Diana Dashynevich <ddashinevich@gmail.com>
2024-12-03 15:18:46 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Diana Dashinevich on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
